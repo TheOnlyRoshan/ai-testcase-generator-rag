239429981344121901f4046b0c6e325b069867ad
Roshan Pandey <roshanpandey@Roshans-MacBook-Pro.local>
2026-07-17 16:11:20 +0530

    improving the project

--- loaders/jira_loader.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class JiraLoader:
-
-    def load(self, jira_url):
-
-        response = requests.get(jira_url)
-
-        return response.text
--- models/testcase_model.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class TestCase:
-
-    id: str
-    scenario: str
-    steps: str
-    expected_result: str
